cfd554f3466b4c20718300e0ecc232da7bea7840
nextacademy <nextacademy@email.com>
2015-10-12 14:51:55 +0800

    able to CRUD

--- app/controllers/posts.rb
+++ app/controllers/posts.rb
@@ -10,15 +10,22 @@ end
 
 #create
 post '/posts' do
+  redirect to 'posts/1'
 end
 
 
 #edit
-post '/posts/:id/edit' do
+get '/posts/:id/edit' do
+  erb :'posts/edit'
 end
 
 #update & delete
 post '/posts/:id' do
+  if params[:decision] == 'edit'
+    redirect to 'posts/1'
+  elsif params[:decision] == 'delete'
+    redirect to '/'
+  end
 
 end
 
---
+++ db/migrate/20151012125312_create_posts.rb
@@ -0,0 +1,6 @@
+class CreatePosts < ActiveRecord::Migration
+	def change
+
+
+	end
+end
